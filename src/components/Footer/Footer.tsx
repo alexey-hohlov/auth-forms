db7d5cd8ba7a231e3d6dd87c15a814483a3a12e5
Alexey Hohlov <alexey.hohl0v@yandex.ru>
2024-02-11 23:55:08 +0700

    ref: footer refactor

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -4,7 +4,7 @@ import styles from './Footer.module.scss';
 const Footer: React.FC = () => {
     return (
         <footer className={styles.footer}>
-            <div className={styles.text}>
+            <div>
                 Built by <span>Alexey Hohlov</span> ©2024
             </div>
             <a
