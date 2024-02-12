b1d27440000505ffd0cd133144c88be4c5d13175
Alexey Hohlov <alexey.hohl0v@yandex.ru>
2024-02-12 23:33:08 +0700

    fix: fix routes for deploy build

--- src/App/App.tsx
+++ src/App/App.tsx
@@ -5,7 +5,7 @@ import { Layout, Login, PasswordReset, SignUp } from '../pages';
 function App() {
     const router = createBrowserRouter([
         {
-            path: '/',
+            path: 'auth-forms',
             element: <Layout />,
             children: [
                 { element: <Greetings />, index: true },
